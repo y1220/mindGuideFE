a79b72d9c05aeedb365abc75ac04b28535d55e5e
y1220 <ga.2.y1220@gmail.com>
2024-10-29 00:21:34 +0100

    feat(DynamicEmoji): utilize assigned emotion from gemini response

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,3 +1,5 @@
+require 'active_support/json'
+
 class ChatController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :chat
   def home
@@ -7,6 +9,9 @@ class ChatController < ApplicationController
     end
     if params[:voice_input].present?
       @voice = params[:voice_input]
+    end
+    if params[:emotional_type].present?
+      @emotional_type = params[:emotional_type]
     end
   end
 
@@ -23,23 +28,56 @@ class ChatController < ApplicationController
       render json: { error: 'Configuration file not found' }, status: :internal_server_error
       return
     end
+    valid_emotions = ['smile', 'smile_with_big_eyes', 'grin', 'laughing', 'joy', 'star_struck',
+              'kissing_heart', 'kissing', 'kissing_smiling_eyes', 'partying_face', 'Pleading',
+              'thinking_face', 'zipper_mouth', 'screeming', 'rage', 'sweat', 'surprised',
+              'scrunched_mouth', 'mind_blown', 'cry', 'yawn']
 
-    response = GenerativeAiClient.new.generate_content(voice + ", please answer concisely with a few sentences.")
-    content_text = nil
+    prompt = "Can you reply to message with Json object format,
+       containing followings,
+       {\"response\": reply to my question, \"emotional_type\": emotion type name among
+       #{valid_emotions} },
+       for the given sentence and please answer concisely with a few sentences and ask a question to continue our conversation: '#{voice}'"
+    response = GenerativeAiClient.new.generate_content(prompt)
+    response_text = nil
     if response
       content_text = response.dig("candidates", 0, "content", "parts", 0, "text")
+      puts content_text
+      if valid_json?(content_text)
+        response_data = ActiveSupport::JSON.decode(content_text)
+        response_text = response_data['response']
+        emotional_type = response_data['emotional_type']
+        unless valid_emotions.include?(emotional_type)
+          emotional_type = "zipper_mouth"
+        end
+      else
+        response_text = "Invalid JSON response"
+        emotional_type = "zipper_mouth"
+      end
     else
-      content_text = "Failed to generate content"
+      response_text = "Failed to generate content"
+      emotional_type = "zipper_mouth"
     end
-    description = content_text
+    description = response_text
     # file = File.read(file_path)
     # emoji_descriptions = JSON.parse(file)
     # description = emoji_descriptions[emotion]['description']
 
     if description
-      redirect_to action: 'home', chat_response: description, voice_input: voice
+      redirect_to action: 'home', chat_response: description, voice_input: voice, emotional_type: emotional_type
     else
       render json: { error: 'Emotion label not found' }, status: :not_found
+    end
+  end
+
+  private
+
+  def valid_json?(json_string)
+    begin
+      ActiveSupport::JSON.decode(json_string)
+      true
+    rescue ActiveSupport::JSON.decode
+      false
     end
   end
 end
